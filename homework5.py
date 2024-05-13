f37bc22e7a53de4cb4aa9380d6119683fc014e4d
V1adimir-Ragozin <vragozin1995@gmail.com>
2024-05-13 21:57:30 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_list = ['вишня', 'дыня', 'банан', 'яблоко', 'груша']
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'кукуруза'
+print(my_list)
+my_dict = {'вишня': 'cherry', 'дыня': 'melon', 'кукуруза': 'corn', 'яблоко': 'apple', 'груша': 'pear'}
+print(my_dict)
+print(my_dict['кукуруза'])
+my_dict['кукуруза'] = 'Corne'   # Изменил значение ключа
+my_dict['личи'] = 'lychee'     # Добавил новый ключ
+print(my_dict)
